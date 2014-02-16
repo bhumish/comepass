aa37818e2c22ea74a013dbb61e0bc015fc5a1afc
Bhumish Gajjar <bhumish@users.noreply.github.com>
2014-02-16 17:57:31 +0530

    Created setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+	name  		  = 'comepass',
+	version		  = '1.0.0',
+	py_modules	= ['comepass'],
+	author		  = 'Bhumish Gajjar',
+	author_email= 'bhumish.gajjar@gmail.com',
+	url		      = 'https://github.com/bhumish',
+	description	= 'For securely storing and managing passwords',
+)
